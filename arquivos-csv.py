8f0238f141f3a94e4239f334838864cb73e6e86e
Tu Nombre <rapsful@gmail.com>
2026-04-19 13:52:45 -0300

    arquivos csv

---
+++ arquivos-csv.py
@@ -0,0 +1,13 @@
+import csv
+from pathlib import Path
+file_path = Path("Files/sample_data.csv")
+lista : list = []
+with open(file=file_path, mode='r',encoding='utf-8',newline='') as f:
+    reader = csv.DictReader(f)
+
+    for line in reader:
+        lista.append(line)
+        
+
+
+print(lista)
